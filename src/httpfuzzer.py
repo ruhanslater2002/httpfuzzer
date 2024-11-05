fb8b83ec5b869fa66845877f1f9dbd28dffb3511
Ruhan <ruhan.slater@gmail.com>
2024-11-05 13:01:13 +0200

    Added threading

--- src/httpfuzzer.py
+++ src/httpfuzzer.py
@@ -4,13 +4,14 @@ import time
 
 
 class HttpFuzzer:
-    def __init__(self, wordlist: str, url: str):
+    def __init__(self, wordlist: str, url: str, threading: float):
         # Init colors
         self.plus = colored("+", "green")
         self.minus = colored("-", "red")
 
         self.wordlist = wordlist
         self.url = url
+        self.threading = threading
 
     def fuzz_hidden_directories(self):
         with open(self.wordlist, "r") as wordlist:
@@ -26,7 +27,7 @@ class HttpFuzzer:
                 httphandler: HttpHandler = HttpHandler(full_url)
                 try:
                     # Gets status code from combined url
-                    time.sleep(0.3)  # Introduce a delay
+                    time.sleep(self.threading)  # Introduce a delay
                     response = httphandler.get_status_code()  # Make sure this matches your method
                     if response == 200:
                         print(f"[{self.plus}] Response {response} from {colored(full_url, "green")}")
--- src/main.py
+++ src/main.py
@@ -15,16 +15,17 @@ ___________
 
 
 class Main:
-    def __init__(self, wordlist: str, url: str):
+    def __init__(self, wordlist: str, url: str, threading: float):
         self.url = url
         self.wordlist = wordlist
+        self.threading = threading
 
     def start(self):
         print(f"[*] Fuzzing {self.url}")
-        httpfuzzer: HttpFuzzer = HttpFuzzer(self.wordlist, self.url)
+        httpfuzzer: HttpFuzzer = HttpFuzzer(self.wordlist, self.url, self.threading)
         httpfuzzer.fuzz_hidden_directories()
 
 
 if __name__ == '__main__':
     print(ascii_art())
-    Main("wordlists/subdomains-top1million.txt", "http://192.168.253.134").start()
+    Main("wordlists/subdomains-top1million.txt", "http://192.168.253.134", 0.1).start()
